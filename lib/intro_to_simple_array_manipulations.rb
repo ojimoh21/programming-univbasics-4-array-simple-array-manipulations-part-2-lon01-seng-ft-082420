7e0c753149eda7b2b6389a9c39ab4dd2342d988e
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-01 20:34:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,5 +14,12 @@ def using_flatten(array)
   array.flatten()
 end 
 
-def using_delete
+def using_delete(array, string)
+  array.delete(string)
 end 
+
+def using_delete_at(array, integer)
+  array.delete(integer)
+end 
+
+def 
